f4778ab12468ca066b6f157e7024cfbcca84969a
gavirlenkoartem038 <gavrilenkoartem038@gmail.com>
2023-06-29 19:51:26 +0300

    feat: add form validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//burger
 const burger = document.querySelector('.burger');
 const cross = document.querySelector('.cross');
 const nav = document.querySelector('.nav');
@@ -16,3 +17,61 @@ nav.addEventListener('click', function (e) {
     togglenMenu('remove');
   }
 });
+
+//form validation
+
+const formElements= document.querySelectorAll('.input');
+const inputFields = document.querySelectorAll('.input__field');
+const form = document.querySelector('.form');
+
+const validateText = (value) => {
+  if (value.length >=2 && value.length <= 50) return true;
+  return false;
+}
+
+const validateEmail = (value) => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(value);
+}
+
+const validateArea = (value) => {
+  if (value.length >=2 && value.length <= 100) return true;
+  return false;
+}
+
+const validateElement = (element) => {
+  const type = element.type;
+  const value = element.value;
+  switch (type) {
+    case 'text':
+      return validateText(value);
+    case 'email':
+      return validateEmail(value);
+    case 'textarea':
+      return validateArea(value);
+  }
+}
+
+const validateForm = () =>  {
+  formElements.forEach(el => el.classList.remove('invalid'));
+  const inputFieldsArray = Array.from(inputFields);
+  const invalidElements = inputFieldsArray.filter((el) => !validateElement(el));
+
+  invalidElements.forEach((el) => el.parentElement.classList.add('invalid'));
+
+  return invalidElements.length === 0;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  inputFields.forEach(el => el.addEventListener('input', validateForm ));
+  if (validateForm()) {
+    clearFields();
+  };
+});
+
+const clearFields = () => {
+  inputFields.forEach(el => el.removeEventListener('input', validateForm ));
+  inputFields.forEach(el => el.value = '')
+  console.log('valid');
+}
